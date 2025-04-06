063c3e2e296c3ee7642bfdb5b19612a642b0fa73
Akib Bagwan <akibbagwan69@gmail.com>
2025-04-06 19:00:40 +0530

    removed unused resources

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "./OurServices.module.css"
-import { education } from '../../data/constants'
+import { services } from '../../data/constants'
 import { VerticalTimeline }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import Cards from './OurServicesCard/Cards';
@@ -11,7 +11,7 @@ const Education = () => {
             <p className={styles.title}>Our Services</p>
             <p className={ styles.description}>Our Client Services Team is always happy to talk to you about what is best for your needs. Contact our team to find out if we are the right fit for you.</p>
             <VerticalTimeline>
-                {education.map((education,idx) =>
+                {services.map((education,idx) =>
                     < Cards education={education} index={ idx} />
                 )}
             </VerticalTimeline></div>
